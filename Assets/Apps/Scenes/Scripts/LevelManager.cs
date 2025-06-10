bdf170c91b5681f7011609fd56f49bb0394ff844
Pimodorf16 <158831496+Pimodorf16@users.noreply.github.com>
2025-06-10 21:54:32 +0700

    Scene Transition

---
+++ Assets/Apps/Scenes/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(Load(sceneName));
+    }
+
+    IEnumerator Load(string sceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
